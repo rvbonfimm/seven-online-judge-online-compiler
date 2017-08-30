1d7f701f07def8e52a01345560b83b7afe35618b
Rogerio Vilela Bonfim <rogeriovilelabonfim@gmail.com>
2017-08-29 21:42:59 -0300

    Alteracoes gerais no script main do flask

--- web/app.py
+++ web/app.py
@@ -1,110 +1,350 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
 import subprocess
 import os
 import time
 import pexpect
+import json
 
 from flask import Flask, render_template, request, url_for
+from flaskext.mysql import MySQL
 from pexpect import *
+from flask_bcrypt import Bcrypt
+
+
+mysql = MySQL()
 
 app = Flask(__name__)
+
+bcrypt = Bcrypt(app)
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'rogerioo4265416'
+app.config['MYSQL_DATABASE_DB'] = 'sevenonlinejudge'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql.init_app(app)
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('main.html')
+
+    title = "Seven Online Judge"
+
+    return render_template('index.html', title=title)
 
 
-@app.route('/dashboard')
-def dashboard():
-	return render_template('dashboard.html')
+@app.route('/main')
+def main():
+
+    title = "Pagina principal"
+
+    return render_template('main.html', title=title)
 
 
-@app.route('/registerUser', methods=['GET','POST'])
-def registerUser():
-	
-	if request.method == 'POST':
+@app.route('/judges')
+def judges():
 
-		name = request.form.get('name')
-		lastname = request.form.get('lastname')
-		email = request.form.get('email')
-		username = request.form.get('username')
-		password = request.form.get('password')
+    title = "Pagina de Julgamento"
 
-	return str(codearea)
+    return render_template('judge_exercise.html', title=title)
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    title = "Pagina de Acesso"
+
+    if (request.method == 'POST'):
+
+        username = request.form['loginField']
+
+        password = request.form['passwordField']
+
+        conn = mysql.connect()
+
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * from users WHERE username = '" + username + "' AND password = '" + password + "'")
+
+        query_result = []
+
+        user_ip = request.environ['REMOTE_ADDR']
+
+        id_user = []
+
+        name = []
+
+        for item in cursor.fetchall():
+
+            id_user = item[0]
+
+            name = item[1]
+
+            query_result.append(id_user)
+
+            query_result.append(name)
+
+        cursor.close()
+
+        if query_result:
+
+            connection = mysql.get_db()
+
+            cursor = connection.cursor()
+
+            query = ("DELETE FROM session WHERE id_user = %s")
+
+            params = (id_user)
+
+            cursor.execute(query, params)
+
+            connection.commit()
+
+            query = ("INSERT INTO session (user_logged, user_ip, id_user) VALUES (%s, %s, %s)")
+
+            params = (name, user_ip, id_user)
+
+            cursor.execute(query, params)
+
+            connection.commit()
+
+            cursor.close()
+
+            message = "Usuario logado com sucesso."
+
+            return render_template('main.html', title=title, message=message)
+
+        else:
+
+            message = "Usuario ou Senha invalidos. Confira os dados digitados e tente novamente!"
+
+            return render_template('index.html', message=message)
+
+    elif (request.method == 'GET'):
+
+        return render_template('index.html', title=title)       
+
+
+@app.route('/logout', methods=['POST',''])
+def logout():
+
+    connection = mysql.get_db()
+
+    cursor = connection.cursor()
+
+    query = ("DELETE FROM session WHERE id_user = %s")
+
+    params = (id_user)
+
+    cursor.execute(query, params)
+
+    connection.commit()
+
+    cursor.close()
+
+    message = "Usuario deslogado com sucesso."
+
+    return render_template('index.html', message=message)
+
+
+@app.route('/newUser', methods=['GET','POST'])
+def newUser():
+    
+    title = "Pagina de Novo acesso"
+
+    if request.method == 'POST':
+
+        name = request.form['nameField']
+
+        lastname = request.form['lastnameField']
+
+        email = request.form['emailField']
+
+        username = request.form['usernameField']
+
+        password = request.form['passwordField']
+
+        gender = request.form['gender']
+
+        if not ( name and lastname and email and username and password and gender):
+
+            message = "Voce esqueceu de digitar algum dos campos obrigatorios.\n"
+
+            return render_template('index.html', message=message)
+
+        connection = mysql.get_db()
+
+        cursor = connection.cursor()
+
+        query = ("INSERT INTO users(name, lastname, email, username, password, gender) VALUES (%s, %s, %s, %s, %s, %s)")
+
+        params = (name, lastname, email, username, password, gender)
+
+        cursor.execute(query, params)
+
+        connection.commit()
+
+        message = "Novo Usuario cadastrado com sucesso!\n Let's code, dude!\n"
+
+        return render_template('index.html', message=message)
+
+    elif request.method == 'GET':
+
+        return render_template('newUser.html', title=title)         
+
+
+@app.route('/exercises')
+def exercises():
+
+    title = "Pagina de Exercicios"
+
+    return render_template('judge_exercises.html', title=title)
+
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+
+    title = "Pagina de Novo Usuario"
+
+    return render_template('newUser.html', title=title)
+
+
+@app.route('/julgar')
+def judge():
+
+    return render_template('judge.html')
+
+
+@app.route('/admin/registerExercise', methods=['GET','POST'])
+def registerExercise():
+
+    if(request.method == 'POST'):
+
+        connection = mysql.get_db()
+
+        cursor = connection.cursor()
+
+        exercise_number = request.form.get('exercise_number')
+
+        name = request.form.get('name')
+
+        description = request.form.get('description')
+
+        level = request.form.get('level')
+
+        inputt = request.form.get('input')
+
+        output = request.form.get('output')
+
+        query = ("INSERT INTO exercises (exercise_number, name, description, level, input, output) VALUES (%s, %s, %s, %s, %s, %s)")
+
+        params = (exercise_number, name, description, level, inputt, output)
+
+        cursor.execute(query,params)
+
+        connection.commit()
+
+        cursor.close()
+
+        message = "New exercise was successfully inserted at the bank.\n"
+
+        return render_template('index.html', message=message)
+
+    elif(request.method == 'GET'):
+
+        return render_template('create_exercise.html')
+    
 
 @app.route('/registerCode', methods=['POST'])
 def registerCode():
 
-	USER_CODE = request.form.get('codearea')
+    title = "Julgamento de Exercicios"
 
-	EXERCISE_NUMBER = request.form.get('exercise_number')
+    exercise_number = request.form.get('exercise_number')
 
-	LANGUAGE = request.form.get('language')
+    language = request.form.get('language')
 
-	if LANGUAGE == "Python":
+    user_code = request.form.get('codearea')
 
-		LANGUAGE = "py"
+    user_ip = request.environ['REMOTE_ADDR']
 
-	elif LANGUAGE == "C":
+    connection = mysql.get_db()
 
-		LANGUAGE = "c"
+    cursor = connection.cursor()
 
-	DATE_TIME = (time.strftime("%d%m%Y") + "_" + time.strftime("%H%M%S") )
+    query = ("SELECT id FROM exercises WHERE exercise_number = %s")
 
-	FILE_DIR = ((os.getcwd().replace("web", "")) + "compiler/")
+    params = (exercise_number)
 
-	COMPILER_DIR = FILE_DIR + "compiler.py"
+    cursor.execute(query,params)
 
-	USER_FILE = str(FILE_DIR + EXERCISE_NUMBER + "_" + DATE_TIME + "." + LANGUAGE)
+    aux = cursor.fetchall()[0][0]
 
-	#CMD = ["python", COMPILER_DIR, USER_FILE, '-d']
+    id_exercise = int(aux)
 
-	CMD = ("python " + str(COMPILER_DIR) + " " + str(USER_FILE) + " -d ")
+    query = ("SELECT id_user FROM session WHERE user_ip = %s")
 
-	print("[App] Code: " + str(USER_CODE))
-	print("[App] Exercise: " + str(EXERCISE_NUMBER))
-	print("[App] Language: " + str(LANGUAGE))
-	print("[App] Date time: " + str(DATE_TIME))
-	print("[App] File dir: " + str(FILE_DIR))
-	print("[App] Compiler dir: " + str(COMPILER_DIR))
-	print("[App] User file: " + str(USER_FILE))
-	print("[App] Cmd: " + str(CMD))
+    params = (user_ip)
 
-	with open(USER_FILE,"w") as userFile:
+    cursor.execute(query, params)   
 
-		userFile.write(USER_CODE)
+    for item in cursor.fetchall():
 
-		userFile.close()
+        id_user = int(item[0])
 
-	userCodeOut = pexpect.spawn(CMD, timeout=5)
+    query = ("INSERT INTO judges(code, language, id_exercise, id_user) VALUES (%s, %s, %s, %s)")
 
-	checkExpectOut = userCodeOut.expect([pexpect.EOF, pexpect.TIMEOUT])
-	
-	if(checkExpectOut == 1 ):
+    params = (user_code, language, id_exercise, id_user)
 
-		print("TIMEOUT")
+    cursor.execute(query, params)
 
-	pexpectOut = userCodeOut.before
+    connection.commit()
 
-	print(pexpectOut)	
+    cursor.close()
 
-	#process = subprocess.Popen(CMD, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+    # Start to create the user code file to compile
+    date_time = (time.strftime("%d%m%Y") + "_" + time.strftime("%H%M%S") )
 
-	#process.wait()
+    base_compiler_dir = (os.getcwd().replace("web", "")) + "compiler/"
 
-	#process.communicate()[0]
-	
-	#if(process.returncode == 1):
+    exec_dir = base_compiler_dir + "compiler.py"
 
-	#	print("[App] Not ran")
+    user_file_dir = base_compiler_dir + "tojudge/" + exercise_number + "_" + date_time + "_" + str(id_user) + "." + language
 
-	#else:
+    user_file_dir = str(user_file_dir)
 
-	#	print("[App] Ran")
+    with open(user_file_dir,"w") as userFile:
 
-	return render_template('dashboard_result.html')
+        userFile.write(user_code)
+
+        userFile.close()
+
+    cmd = ("python " + str(exec_dir) + " " + str(user_file_dir))
+
+    userCodeOut = pexpect.spawn(cmd, timeout=7)
+
+    index = userCodeOut.expect([pexpect.EOF, pexpect.TIMEOUT])
+
+    if(index == 1 ):
+
+        print("TIMEOUT")
+
+        return "Timeout at web pexpect\n"
+
+    result = str(userCodeOut.before)
+
+    if(result.find('Status 1') != -1):
+
+        result = "Status 1"
+
+
+    print ("[WEB]" + str(result))
+
+    return render_template('exercise_result.html', title=title, result=result)
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+
+    app.run(debug=True)
