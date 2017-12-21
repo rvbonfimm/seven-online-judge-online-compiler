481a153ea4020bc8dc0c935c303c7a6be90620d7
Rogerio Vilela Bonfim <rogeriovilelabonfim@gmail.com>
2017-12-21 20:20:27 -0200

    Change the database password by security

--- web/config.py
+++ web/config.py
@@ -2,7 +2,7 @@ import os
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'mysql://root:rogerioo4265416@localhost/sevenonlinejudge'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:mysecuritypass1234@localhost/sevenonlinejudge'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 SECRET_KEY = os.urandom(24)
